0a53e8bdfb809356e672b10a2f4be61c46623eb9
funkykatas <funkykatas@gmail.com>
2018-09-29 18:45:56 +0300

    Beautified algorithm and tests

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,62 +1,96 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 class GildedRose {
+
     Item[] items;
 
-    public GildedRose(Item[] items) {
+    enum ItemType {
+
+        SIMPLE("", ItemType::updateSellInSimple, ItemType::updateQualitySimple),
+        BRIE("Aged Brie", ItemType::updateSellInSimple, ItemType::updateQualityBrie),
+        SULFURAS("Sulfuras, Hand of Ragnaros", ItemType::updateSellInSulfuras, ItemType::updateQualitySulfuras),
+        TICKET("Backstage passes to a TAFKAL80ETC concert", ItemType::updateSellInSimple, ItemType::updateQualityTicket),
+        CONJURED("Conjured Mana Cake", ItemType::updateSellInSimple, ItemType::updateQualityConjured);
+
+        private final String name;
+        private final Function<Integer, Integer> updateSellInFunction;
+        private final BiFunction<Integer, Integer, Integer> updateQualityFunction;
+
+        ItemType(String name,
+                 Function<Integer, Integer> updateSellInFunction,
+                 BiFunction<Integer, Integer, Integer> updateQualityFunction) {
+
+            this.name = name;
+            this.updateSellInFunction = updateSellInFunction;
+            this.updateQualityFunction = updateQualityFunction;
+        }
+
+        void updateItem(Item item) {
+            item.quality = updateQualityFunction.apply(item.quality, item.sellIn);
+            item.sellIn = updateSellInFunction.apply(item.sellIn);
+        }
+
+        static ItemType findTypeByName(String name) {
+            return Arrays.stream(values())
+                    .filter(i -> i.name.equals(name))
+                    .findAny()
+                    .orElse(SIMPLE);
+        }
+
+        private static int updateSellInSimple(int sellIn) {
+            return sellIn - 1;
+        }
+
+        private static int updateSellInSulfuras(int sellIn) {
+            return sellIn;
+        }
+
+        private static int updateQualitySimple(int quality, int sellIn) {
+            int updatedQuality = sellIn > 0 ? quality - 1 : quality - 2;
+            return updatedQuality > 0 ? updatedQuality : 0;
+        }
+
+        private static int updateQualityBrie(int quality, int sellIn) {
+            int updatedQuality = sellIn > 0 ? quality + 1 : quality + 2;
+            return updatedQuality < 50 ? updatedQuality : 50;
+        }
+
+        private static int updateQualitySulfuras(@SuppressWarnings("unused") int quality,
+                                                 @SuppressWarnings("unused") int sellIn) {
+            return 80;
+        }
+
+        private static int updateQualityTicket(int quality, int sellIn) {
+            int updatedQuality;
+            if (sellIn > 10) {
+                updatedQuality = quality + 1;
+            } else if (sellIn > 5) {
+                updatedQuality = quality + 2;
+            } else if (sellIn > 0) {
+                updatedQuality = quality + 3;
+            } else {
+                updatedQuality = 0;
+            }
+            return updatedQuality < 50 ? updatedQuality : 50;
+        }
+
+        private static int updateQualityConjured(int quality, int sellIn) {
+            int updatedQuality = sellIn > 0 ? quality - 2 : quality - 4;
+            return updatedQuality > 0 ? updatedQuality : 0;
+        }
+
+    }
+
+    GildedRose(Item[] items) {
         this.items = items;
     }
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
+    void updateItems() {
+        Arrays.stream(items).forEach(i -> ItemType.findTypeByName(i.name).updateItem(i));
     }
+
 }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -18,7 +18,7 @@ class GildedRoseTest {
         Item simpleItem = simpleItem(1, 1);
         Item sulfuras = sulfuras(1, 1);
         GildedRose app = new GildedRose(new Item[] { simpleItem, sulfuras });
-        app.updateQuality();
+        app.updateItems();
         assertEquals(simpleItem.name, app.items[0].name);
         assertEquals(sulfuras.name, app.items[1].name);
     }
@@ -26,7 +26,7 @@ class GildedRoseTest {
     @Test
     void updateQuality_emptyArrayNoException() {
         GildedRose app = new GildedRose(new Item[] {});
-        app.updateQuality();
+        app.updateItems();
     }
 
     @ParameterizedTest
@@ -34,30 +34,55 @@ class GildedRoseTest {
     void updateQuality_OneSizeArrayUpdate(Item beforeUpdate, Item afterUpdateExpected) {
 
         GildedRose app = new GildedRose(new Item[] { beforeUpdate });
-        app.updateQuality();
+        app.updateItems();
         Item afterUpdateActual = app.items[0];
 
         assertItemEquals(afterUpdateExpected, afterUpdateActual);
     }
 
     private static Stream<Arguments> createTestItems() {
-        return Stream.of(
+        return Stream
+                .of(
 
+                        createSimpleItemArguments(),
+                        createBrieArguments(),
+                        createSulfurasArguments(),
+                        createTicketArguments(),
+                        createConjuredArguments()
+
+                ).reduce(Stream::concat)
+                .orElseGet(Stream::empty);
+    }
+
+    private static Stream<Arguments> createSimpleItemArguments() {
+        return Stream.of(
                 of(simpleItem(10, 10), simpleItem(9, 9)),
                 of(simpleItem(10, 0), simpleItem(9, 0)),
                 of(simpleItem(1, 1), simpleItem(0, 0)),
                 of(simpleItem(0, 2), simpleItem(-1, 0)),
                 of(simpleItem(-1, 1), simpleItem(-2, 0)),
-                of(simpleItem(-1, 4), simpleItem(-2, 2)),
+                of(simpleItem(-1, 4), simpleItem(-2, 2))
+        );
+    }
 
+    private static Stream<Arguments> createBrieArguments() {
+        return Stream.of(
                 of(brie(10, 1), brie(9, 2)),
                 of(brie(10, 50), brie(9, 50)),
                 of(brie(0, 10), brie(-1, 12)),
-                of(brie(-1, 10), brie(-2, 12)),
+                of(brie(-1, 10), brie(-2, 12))
+        );
+    }
 
+    private static Stream<Arguments> createSulfurasArguments() {
+        return Stream.of(
                 of(sulfuras(10, 80), sulfuras(10, 80)),
-                of(sulfuras(-1, 80), sulfuras(-1, 80)),
+                of(sulfuras(-1, 80), sulfuras(-1, 80))
+        );
+    }
 
+    private static Stream<Arguments> createTicketArguments() {
+        return Stream.of(
                 of(ticket(0, 10), ticket(-1, 0)),
                 of(ticket(-1, 10), ticket(-2, 0)),
                 of(ticket(1, 10), ticket(0, 13)),
@@ -65,8 +90,12 @@ class GildedRoseTest {
                 of(ticket(10, 10), ticket(9, 12)),
                 of(ticket(10, 49), ticket(9, 50)),
                 of(ticket(11, 10), ticket(10, 11)),
-                of(ticket(11, 50), ticket(10, 50)),
+                of(ticket(11, 50), ticket(10, 50))
+        );
+    }
 
+    private static Stream<Arguments> createConjuredArguments() {
+        return Stream.of(
                 of(conjured(10, 10), conjured(9, 8)),
                 of(conjured(0, 10), conjured(-1, 6))
         );
